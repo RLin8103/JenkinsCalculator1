c43117609a487896a9d7bc2cb0e8fbf41e1e0788
Ray Lin <RLin8103@gmail.com>
2023-03-03 17:36:23 -0700

    Add scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * A simple main class for running the spelling corrector. This class is not
@@ -10,11 +11,19 @@ public class Main {
 	 * Give the dictionary file name as the first argument and the word to correct
 	 * as the second argument.
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main() throws IOException {
 
-		String operation = args[0];
-		String inputOne = args[1];
-		String inputTwo = args[2];
+		Scanner scanner = new Scanner(System.in);
+
+		String operation = "";
+		int inputOne = 0;
+		int inputTwo = 0;
+
+		while (scanner.hasNext()) {
+			operation = scanner.next();
+			inputOne = scanner.nextInt();
+			inputTwo = scanner.nextInt();
+		}
 
 		Calculator calculator = new Calculator();
 
