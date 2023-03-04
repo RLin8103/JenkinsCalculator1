ced7cf94e2779b030fd92091f8c99db71753ec3c
Ray Lin <RLin8103@gmail.com>
2023-03-03 17:50:32 -0700

    Fix while loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,34 +23,35 @@ public class Main {
 			operation = scanner.next();
 			inputOne = scanner.nextInt();
 			inputTwo = scanner.nextInt();
-		}
 
-		Calculator calculator = new Calculator();
 
-		if (operation.equals("add")) {
+			Calculator calculator = new Calculator();
 
-			System.out.println(calculator.add(inputOne, inputTwo));
+			if (operation.equals("add")) {
 
-		} else if (operation.equals("subtract")) {
+				System.out.println(calculator.add(inputOne, inputTwo));
 
-			System.out.println(calculator.subtract(inputOne, inputTwo));
+			} else if (operation.equals("subtract")) {
 
-		} else if (operation.equals("multiply")) {
+				System.out.println(calculator.subtract(inputOne, inputTwo));
 
-			System.out.println(calculator.multiply(inputOne, inputTwo));
+			} else if (operation.equals("multiply")) {
 
-		} else if (operation.equals("divide")) {
+				System.out.println(calculator.multiply(inputOne, inputTwo));
 
-			System.out.println(calculator.divide(inputOne, inputTwo));
+			} else if (operation.equals("divide")) {
 
-		} else if (operation.equals("fibonacci")) {
+				System.out.println(calculator.divide(inputOne, inputTwo));
 
-			System.out.println(calculator.fibonacciNumberFinder(inputOne));
+			} else if (operation.equals("fibonacci")) {
 
-		} else if (operation.equals("binary")) {
+				System.out.println(calculator.fibonacciNumberFinder(inputOne));
 
-			System.out.println(calculator.intToBinaryNumber(inputOne));
+			} else if (operation.equals("binary")) {
 
+				System.out.println(calculator.intToBinaryNumber(inputOne));
+
+			}
 		}
 
 	}
